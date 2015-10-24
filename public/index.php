c88e4554144c969cd2f950cedff49c967efabcb3
k1T4eR <k1T4eR@gmail.com>
2015-10-24 21:38:50 +0300

    Slava modifications. Changed check metod in form.

--- public/index.php
+++ public/index.php
@@ -26,21 +26,30 @@ Configuration::set('db.name',           'contacts-book');
 $uri    = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+$isGETMethod    = $method === 'GET';
+$isPOSTMethod   = $method === 'POST';
+
 $pos = strrpos($uri, '?');
 
 if ($pos !== false) {
     $uri = substr($uri, 0, $pos);
 }
 
-$viewParams = [];
+# TODO Trim trailing slash from URI
+
+$viewParams = [
+    'isPOSTMethod' => $isPOSTMethod,
+    'isGETMethod' => $isGETMethod
+];
 global $viewParams;
+
 $name = isset($_POST['Name']) ? $_POST['Name'] : null;
 $email = isset($_POST['Email']) ? $_POST['Email'] : null;
 $password = isset($_POST['Password']) ? $_POST['Password'] : null;
 
 switch ($uri) {
 
-    case (bool)preg_match('/^(\/)$|(users\/?)$/', $uri):
+    case (bool)preg_match('/^\/(users)?$/', $uri):
         $viewParams['view'] = 'list';
         $viewParams['users'] = DatabaseConnection::getInstance()->fetchAll("SELECT * FROM users;");
         $viewParams['deletedUserID'] = isset($_GET['deletedUserID']) ? $_GET['deletedUserID'] : null;
@@ -48,36 +57,39 @@ switch ($uri) {
         $viewParams['addedUserID'] = isset($_GET['addedUserID']) ? $_GET['addedUserID'] : null;
         break;
 
-    case (bool)preg_match('/\/users\/add$\/?/', $uri):
-        $viewParams['view'] = 'form';
-        break;
-
-    case (bool)preg_match('/\/users\/added/', $uri):
-        DatabaseConnection::getInstance()->query("INSERT INTO users (name, email, password) VALUES
+    case (bool)preg_match('/^\/users\/add$/', $uri):
+        if ($isPOSTMethod) {
+            DatabaseConnection::getInstance()->query("INSERT INTO users (name, email, password) VALUES
         ('$name', '$email', '$password');");
-        $user = DatabaseConnection::getInstance()->fetchOne("SELECT id FROM users ORDER BY id DESC LIMIT 1;");
-        header('Location: /users?addedUserID=' . $user->id);
+            $user = DatabaseConnection::getInstance()->fetchOne("SELECT id FROM users ORDER BY id DESC LIMIT 1;");
+            header('Location: /users?addedUserID=' . $user->id);
+        } else {
+            $viewParams['view'] = 'form';
+        }
         break;
 
-    case (bool)preg_match('/\/users\/(\d+)\/(edit|delete|edited)?$/', $uri, $params):
+    case (bool)preg_match('/\/users\/(\d+)\/(edit|delete)?$/', $uri, $params):
         $userID = $params[1];
         $user = DatabaseConnection::getInstance()->fetchOne("SELECT * FROM users WHERE id = $userID;");
 
         if ($user === false) {
             throw new Exception("User with id $userID not found!");
         }
+
+        $viewParams['user'] = $user;
+
         $actionOnUser = (isset($params[2]) ? $params[2] : null) ?: 'show';
 
         switch ($actionOnUser) {
             case 'edit':
-                $viewParams['view'] = 'form';
-                $viewParams['user'] = $user;
-                break;
-            case 'edited':
-
-                DatabaseConnection::getInstance()->execute("UPDATE users
+                if ($isPOSTMethod) {
+                    DatabaseConnection::getInstance()->execute("UPDATE users
    SET name = '$name', email = '$email', password = '$password' WHERE id = $userID");
-                header('Location: /users?editedUserID=' . $user->id);
+                    header('Location: /users?editedUserID=' . $user->id);
+                } else {
+                    $viewParams['view'] = 'form';
+                    $viewParams['user'] = $user;
+                }
                 break;
             case 'delete':
                 DatabaseConnection::getInstance()->execute("DELETE FROM users WHERE id = $userID");
